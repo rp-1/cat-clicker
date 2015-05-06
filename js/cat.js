aaf341768f5bef0bfecfa93c3140d716ef835f76
rp-1 <richpaschall@comcast.net>
2015-05-06 13:08:01 -0500

    Working two cats

--- js/cat.js
+++ js/cat.js
@@ -1,11 +1,56 @@
-var clicker = document.getElementById("clickCount");
-var mainCatImage = document.getElementById("mainCatImage");
+var catSection = document.getElementById("catSection");
 
-var clickCount = 0;
+var CatClick = {
+    "cats": 
+        [
+            {
+                "name": "Bruno",
+                "img": "images/cat1.jpg",
+                "clicks": 0
+            },
+
+            {
+                "name": "Mars",
+                "img": "images/cat5.jpg",
+                "clicks": 0
+            }
+        ]
+};
 
 
-mainCatImage.addEventListener("click", handleClick, false);
+document.on
+    
+for(var i = 0; i < CatClick.cats.length; i++) {
+    var cat = CatClick.cats[i];
+    catSection.innerHTML += "<p>" + cat.name + "</p>";
+    catSection.innerHTML += "<img src='" + cat.img + "' onclick=catClicked(" + i + ") class='mainCatImage'>";
+    catSection.innerHTML += "<p>Click count: <span id='clickCount'>" + cat.clicks + "</span></p>";
 
-function handleClick(evt) {
-    clicker.innerHTML = ++clickCount;
 }
+
+
+function catClicked(index) {
+    CatClick.cats[index].clicks += 1;
+    document.getElementsByTagName("span")[index].innerHTML = CatClick.cats[index].clicks;
+}
+
+    
+
+
+/*
+        {
+            "name": "JJ",
+            "img": "images/cat2.jpg",
+            "clicks": 0
+        },
+        {
+            "name": "Alfafa",
+            "img": "images/cat3.jpg",
+            "clicks": 0
+        },
+        {
+            "name": "Cool Cat",
+            "img": "images/cat4.jpg",
+            "clicks": 0
+        },
+*/  
