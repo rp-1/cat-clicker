eba79568cc37f5360c171df26c09ce4f1c0e26c3
rp-1 <richpaschall@comcast.net>
2015-05-07 14:36:15 -0500

    kinda sorta mvc 5 cat version

--- js/cat.js
+++ js/cat.js
@@ -1,5 +1,5 @@
-var catSection = document.getElementById("catSection");
-
+var catPicSection = document.getElementById("selectedCat");
+var catList = document.getElementById("catList");
 var CatClick = {
     "cats": 
         [
@@ -8,7 +8,21 @@ var CatClick = {
                 "img": "images/cat1.jpg",
                 "clicks": 0
             },
-
+            {
+                "name": "JJ",
+                "img": "images/cat2.jpg",
+                "clicks": 0
+            },
+            {
+                "name": "Alfafa",
+                "img": "images/cat3.jpg",
+                "clicks": 0
+            },
+            {
+                "name": "Cool Cat",
+                "img": "images/cat4.jpg",
+                "clicks": 0
+            },
             {
                 "name": "Mars",
                 "img": "images/cat5.jpg",
@@ -18,39 +32,49 @@ var CatClick = {
 };
 
 
-document.on
+
     
 for(var i = 0; i < CatClick.cats.length; i++) {
     var cat = CatClick.cats[i];
-    catSection.innerHTML += "<p>" + cat.name + "</p>";
-    catSection.innerHTML += "<img src='" + cat.img + "' onclick=catClicked(" + i + ") class='mainCatImage'>";
-    catSection.innerHTML += "<p>Click count: <span id='clickCount'>" + cat.clicks + "</span></p>";
+    
+    // Fill catListSection with line items
+    if(i === 0) {
+        catList.innerHTML += "<li id='selected' onclick=catSelected(" + i + ")>" + cat.name + "</li>";
+    } else {
+        catList.innerHTML += "<li onclick=catSelected(" + i + ")>" + cat.name + "</li>";
+    }
+    
+    /*
+    catPicSection.innerHTML += "<p>" + cat.name + "</p>";
+    catPicSection.innerHTML += "<img src='" + cat.img + "' onclick=catClicked(" + i + ") class='mainCatImage'>";
+    catPicSection.innerHTML += "<p>Click count: <span id='clickCount'>" + cat.clicks + "</span></p>";
+    */
 
 }
 
 
 function catClicked(index) {
     CatClick.cats[index].clicks += 1;
-    document.getElementsByTagName("span")[index].innerHTML = CatClick.cats[index].clicks;
+    // There is only one span tag at any time so we don't use index here
+    document.getElementsByTagName("span")[0].innerHTML = CatClick.cats[index].clicks;
 }
 
-    
+catSelected(0);
 
+function catSelected(index) {
+    var cat = CatClick.cats[index];
+    var list = document.getElementsByTagName("li");
+    for(var i = 0; i < list.length; i++) {
+        if(i === index) {
+            console.log("i is equal to index");
+            list[i].setAttribute("id", "selected");
+        } else {
+            list[i].setAttribute("id", "");
+        }
+    }
+    //list[index].setAttribute("id", "selected");
+    catPicSection.innerHTML = "<p>" + cat.name + "</p>";
+    catPicSection.innerHTML += "<img src='" + cat.img + "' onclick=catClicked(" + index + ") class='mainCatImage'>";
+    catPicSection.innerHTML += "<p>Click count: <span id='clickCount'>" + cat.clicks + "</span></p>";
 
-/*
-        {
-            "name": "JJ",
-            "img": "images/cat2.jpg",
-            "clicks": 0
-        },
-        {
-            "name": "Alfafa",
-            "img": "images/cat3.jpg",
-            "clicks": 0
-        },
-        {
-            "name": "Cool Cat",
-            "img": "images/cat4.jpg",
-            "clicks": 0
-        },
-*/  
+}
